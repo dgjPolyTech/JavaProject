2132624dcc23503c51b37412e751a9d65cefc6a5
dgjPolyTech <dgjpolytech@gmail.com>
2025-03-17 11:51:35 +0900

    main 메소드 사용법

---
+++ src/chapter02/Code01.java
@@ -0,0 +1,12 @@
+package chapter02;
+
+public class Code01 {
+    public static void main(String[] args){
+        System.out.print("AI 소프트");
+        System.out.println("도경진");
+        System.out.printf("프린트 f\n");
+        System.out.printf("result: "+100+200);// result: 100200
+        System.out.printf("result: "+(100+200));//result: 300
+        System.out.println(100+200+"won"); //300won
+    }
+}
