7860c9bb22868583d475e9a95bfccf9b63774847
dgjPolyTech <dgjpolytech@gmail.com>
2025-05-19 09:43:19 +0900

    조건에 따라 종료 가능한 무한 루프

---
+++ src/chapter06/whileCalc.java
@@ -0,0 +1,29 @@
+package chapter06;
+
+import java.util.Scanner;
+
+public class whileCalc {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int sum = 0;
+        int num1 = 0, num2 = 0;
+
+        while(true){
+            System.out.print("숫자1 ==> ");
+            num1 = sc.nextInt();
+
+            if(num1 == 0) {
+                break;
+            }
+
+            System.out.print("숫자2 ==> ");
+            num2 = sc.nextInt();
+
+            sum = num1 + num2;
+            System.out.println(num1+" + "+num2+" = " + sum);
+        }
+
+        System.out.println("프로그램 종료");
+    }
+}
